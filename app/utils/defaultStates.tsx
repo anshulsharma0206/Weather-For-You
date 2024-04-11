f42bc24782654a831b64463c89774404934a3899
anshulsharma0206 <sanshul5555@gmail.com>
2024-04-11 16:15:56 +0530

    final commit

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Delhi",
+    country: "IND",
+    state: "Delhi",
+    lat: 28.7041,
+    lon: 77.2167,
+  },
+  {
+    name: "London",
+    country: "GB",
+    state: "England",
+    lat: 51.5074,
+    lon: 0.1278,
+  },
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "Sydney",
+    country: "AU",
+    state: "New South Wales",
+    lat: -33.8688197,
+    lon: 151.2092955,
+  },
+  {
+    name: "France",
+    country: "FR",
+    state: "France",
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+];
+
+export default defaultStates;
